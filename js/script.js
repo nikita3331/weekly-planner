e9f24b1d58c6d82288d59ce23ce4224514a903a2
Mykyta Brazhynskyy <mykyta.brazhynskyy@gmail.com>
2022-09-21 12:08:01 +0300

    Added filtering of tasks for specific days

--- js/script.js
+++ js/script.js
@@ -1,47 +1,61 @@
 {
-  console.log("mama");
   const tasks = [];
-
-  const render = () => {
-    let htmlTaskString = "";
-
-    const weekElement = document.querySelector(".js-week");
-    const weekContent = weekElement.value;
-
-    for (const task of tasks) {
+  const createHtmlString = (filteredTasks) =>{
+    let htmlTaskString=''
+    for (const task of filteredTasks) {
       htmlTaskString += `
         <li>
         ${task.content}
         </li>`;
     }
-
+    return htmlTaskString
+  }
+  const getTasksForDay=(selectedDay)=>{
+    const filteredTasks=[]
+    for (const task of tasks) {
+      if(task.day===selectedDay){
+        filteredTasks.push(task)
+      }
+    }
+    return filteredTasks
+  }
+  const render = () => {
+    const weekElement = document.querySelector(".js-week");
+    const weekContent = weekElement.value;
     switch (weekContent) {
       case "Monday":
-        document.querySelector(".js-monday").innerHTML = htmlTaskString;
+        let mondayTasks=getTasksForDay('monday')
+        document.querySelector(".js-monday").innerHTML = createHtmlString(mondayTasks);
         break;
 
       case "Tuesday":
-        document.querySelector(".js-tuesday").innerHTML = htmlTaskString;
+        let tuesdayTasks=getTasksForDay('tuesday')
+        document.querySelector(".js-tuesday").innerHTML =  createHtmlString(tuesdayTasks);
         break;
 
       case "Wednesday":
-        document.querySelector(".js-wednesday").innerHTML = htmlTaskString;
+        let wednesdayTasks=getTasksForDay('wednesday')
+        document.querySelector(".js-wednesday").innerHTML =  createHtmlString(wednesdayTasks);
         break;
 
       case "Thursday":
-        document.querySelector(".js-thursday").innerHTML = htmlTaskString;
+        let thursdayTasks=getTasksForDay('thursday')
+        document.querySelector(".js-thursday").innerHTML =  createHtmlString(thursdayTasks);
         break;
 
       case "Friday":
-        document.querySelector(".js-friday").innerHTML = htmlTaskString;
+        let fridayTasks=getTasksForDay('friday')
+        document.querySelector(".js-friday").innerHTML =  createHtmlString(fridayTasks);
         break;
 
       case "Saturday":
-        document.querySelector(".js-saturday").innerHTML = htmlTaskString;
+        let saturdayTasks=getTasksForDay('saturday')
+        document.querySelector(".js-saturday").innerHTML =  createHtmlString(saturdayTasks);
         break;
 
       case "Sunday":
-        document.querySelector(".js-sunday").innerHTML = htmlTaskString;
+        let sundayTasks=getTasksForDay('sunday')
+        document.querySelector(".js-sunday").innerHTML =  createHtmlString(sundayTasks);
         break;
     }
   };
@@ -55,13 +69,15 @@
       event.preventDefault();
 
       const taskElement = document.querySelector(".js-tasks");
+      const dropdown = document.querySelector(".js-week");
+      const selectedDay = dropdown.value;
       const taskContent = taskElement.value.trim();
-
       if (taskContent === "") {
         return;
       }
       tasks.push({
         content: taskContent,
+        day:selectedDay.toLowerCase(),
         done: false,
       });
       render();
